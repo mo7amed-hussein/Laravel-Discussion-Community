0b7198280fb7a358ff682d658d80a57f499facc8
mohamed hussein <m.hussein1389@gmail.com>
2017-12-18 23:08:00 +0200

    create register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,28 @@
 @extends('layout.main')
-@section('title',' | login')
+@section('title',' | register')
 @section('content')
 <div class="row">
 	<div class="col-md-6 col-md-offset-3">
 		<div class="panel panel-default">
 			<div class="panel-heading text-center">
-				Login
+				Sign UP
 			</div>
 			<div class="panel-body">
-			<form action="{{route('login')}}" method="post" class="form-horizontal">
+			<form action="{{route('register')}}" method="post" class="form-horizontal">
 				{{csrf_field()}}
+
+				<div class="form-group">
+					<label class="col-md-4 control-label">Name</label>
+					<div class="col-md-6">
+					<input type="text" name="name" class="form-control" value="{{old('name')}}">
+					@if($errors->has('name'))
+					<span class="help-block">
+						<strong>{{$errors->first('name')}}</strong>
+					</span>
+					@endif
+					</div>
+				</div>
+
 				<div class="form-group">
 					<label class="col-md-4 control-label">Email</label>
 					<div class="col-md-6">
@@ -24,7 +37,7 @@
 				<div class="form-group">
 					<label class=" col-md-4 control-label">Password</label>
 					<div class="col-md-6">
-					<input type="password" name="password" class="form-control" >
+					<input type="password" name="password" class="form-control">
 					@if($errors->has('password'))
 					<span class="help-block">
 						<strong>{{$errors->first('password')}}</strong>
@@ -34,19 +47,18 @@
 				</div>
 
 				<div class="form-group">
-					<div class="col-md-6 col-md-offset-4">
-					<label><input type="checkbox" name="remember" {{old('remember')?'checked':''}}>
-					Remember Me</label>
-					</div>
+					<label class=" col-md-4 control-label">Confirm Password</label>
+					<div class="col-md-6">
+					<input type="password" name="password_confirmation" class="form-control">
+				</div>
 				</div>
 			
 			</div>
 			<div class="panel-footer">
 				<div class="form-group row">
 
-				<div class="col-md-7 col-md-offset-3">
-					<input type="submit" name="submit" value="login" class="btn btn-success">
-					<a href="{{route('password.request')}}" class="btn btn-link">Forgot Your Password?</a>
+				<div class="col-md-6 col-md-offset-3">
+					<input type="submit" name="submit" value="Register" class="btn btn-success btn-block">
 			    </div>
 			    </div>
 			</div>
