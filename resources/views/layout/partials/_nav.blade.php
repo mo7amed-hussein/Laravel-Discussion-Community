ba72d82be887b0d68139189f43d005072af387c5
mohamed hussein <m.hussein1389@gmail.com>
2017-12-18 23:14:35 +0200

    update navbar for logged users

--- resources/views/layout/partials/_nav.blade.php
+++ resources/views/layout/partials/_nav.blade.php
@@ -35,10 +35,11 @@
 
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    test <span class="caret"></span>
+                                    {{Auth::user()->name}} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
+                                    <li><a href="{{route('profile.show',Auth::id())}}">Profile</a></li>
                                     <li>
                                         <a href="#"
                                             onclick="event.preventDefault();
@@ -46,7 +47,7 @@
                                             Logout
                                         </a>
 
-                                        <form id="logout-form" action="#" method="POST" style="display: none;">
+                                        <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
